7e306ae44588de3bdf10ee47d48b05eaf1af808d
zacchaeus-tan <zacchaeus.t.2025@computing.smu.edu.sg>
2026-07-26 22:59:29 +0800

    refresh

--- backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.models.user import User
-from app.models.resume import Resume
-from app.models.session import FlowSession
-from app.models.job import JobSuggestion
-from app.models.grant import Grant, GrantRecommendation
-from app.models.application import Application
-
-__all__ = ["User", "Resume", "FlowSession", "JobSuggestion", "Grant", "GrantRecommendation", "Application"]
